1f0cce1b7bced093922705010012ad9cc630eb2f
Diego R <diegorolimdejesus@gmail.com>
2025-10-22 15:26:24 -0300

    JS

---
+++ exercicios/aula011/ex008.js
@@ -0,0 +1,6 @@
+var vel = 60.5
+console.log(`A velocidade do seu carro é de ${vel}` )
+if (vel > 60) {
+    console.log('Você fou multado por ultrapassar a velocidade permitida')
+}
+console.log('Sempre ande com segurança')
