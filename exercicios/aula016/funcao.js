e32b05f579b3ec4a25b2027342bda65a665cba17
Diego R <diegorolimdejesus@gmail.com>
2025-11-12 10:47:29 -0300

    js

---
+++ exercicios/aula016/funcao.js
@@ -0,0 +1,10 @@
+function parimp(n) {
+    if (n%2 == 0) {
+        return 'par'
+    } else {
+        return 'impar'
+    }
+}
+//let res = parimp(11) isso [é uma opção]
+//console.log(res)
+console.log(parimp(2))
