edabe384b89681c3bfbe22d6d00735c28d88232c
Diego R <diegorolimdejesus@gmail.com>
2026-01-06 11:14:09 -0300

    bump

---
+++ javascript do 0/EX/ex16/js.js
@@ -0,0 +1,14 @@
+function contar(){
+    var ini = document.getElementById('in')
+    var fim = document.getElementById('fim')
+    var pss = document.getElementById('pss') 
+    var res = document.getElementById(res)
+    if (ini.value.length == 0) {
+        alert('coloque algo')
+    }   if (fim.value.length == 0) {
+        alert('coloque algo')
+    } 
+      if (pss.value.length == 0) {
+        alert('coloque algo')
+    } 
+}
