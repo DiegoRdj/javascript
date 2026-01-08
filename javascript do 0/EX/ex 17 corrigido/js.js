4e29fb7aa690ca14a6b895f05c85ac281e53f47a
Diego R <diegorolimdejesus@gmail.com>
2026-01-08 12:07:37 -0300

    bump

--- javascript do 0/EX/ex 17 corrigido/js.js
+++ javascript do 0/EX/ex 17 corrigido/js.js
@@ -1,30 +1,19 @@
-function contar(){
-    var ini = document.getElementById('in')
-    var fim = document.getElementById('fim')
-    var pss = document.getElementById('pss') 
-    var res = document.getElementById('res')
+function tabuada() {
+    var num = document.getElementById('txtn')
+    var tab = document.getElementById('seltab')
+if (num.value.length == 0){
+    alert('digite um number')
 
-    if (ini.value.length == 0|| fim.value.length == 0 || pss.value.length == 0) {
-        alert('coloque algo')
-        return
+} else {
+    var n = Number(num.value)
+    var c = 1
+    tab.innerHTML = ''
+    while (c <= 10) {
+        var item = document.createElement('option')
+        item.text = `${n} x ${c} = ${n*c}`
+        item.value = `tab${c}`
+        tab.appendChild(item)
+        c++
     }
-
-    var inicio = Number(ini.value)
-    var final = Number(fim.value)
-    var passo = Number(pss.value)
-
-    if(passo <= 0) {
-        alert('Passo invalido! sera utilizado passo 1')
-        passo = 1
-    }
-    if (inicio < final) {
-        for (var i = inicio; i <= final; i+= passo){
-            res.innerHTML += `${i} 👉 `
-        }
-    }else{
-        for (var i = inicio; i >= final; i -= passo) {
-            res.innerHTML += `${i} 👉 `
-        }
-    }
-    res.innerHTML += ' 🏁'
+ }
 }
