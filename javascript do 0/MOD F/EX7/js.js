8bfcb2acf34816d291d708126d2e3c185b0fc5ee
Diego R <diegorolimdejesus@gmail.com>
2026-01-14 11:48:58 -0300

    bump

--- javascript do 0/MOD F/EX7/js.js
+++ javascript do 0/MOD F/EX7/js.js
@@ -4,22 +4,49 @@ var res = document.getElementById('res')
 var valores = []
 
 function isNumero(n){
-    if (Number(n) >= 1 && Number(n) <= 100){
-        return true
-    } else {
-        return false
-    }
+    return Number(n) >= 1 && Number(n) <= 100
+}
 
-   function inLista(n, l){
-    if (l.indexOf(Number(n)) != -1){
-        return true
-    } else {
-        return false
-    }
-}   
+function inLista(n, l){
+    return l.indexOf(Number(n)) != -1
+}
+
 function adicionar() {
     if (isNumero(num.value) && !inLista(num.value, valores)) {
-
+        valores.push(Number(num.value))
+        var item = document.createElement('option')
+        item.text = `Valor ${num.value} adicionado`
+        lista.appendChild(item)
+        res.innerHTML = ''
+    } else {
+        alert('valor já inserido ou inválido')
     }
+    num.value = ''
+    num.focus()
 }
+
+function finalizar() {
+    if (valores.length == 0){
+        alert('coloca algo')
+    } else {
+        var total = valores.length
+        var maior = valores[0]
+        var menor = valores[0]
+        var soma  = 0
+
+        for (var pos in valores) {
+            soma += valores[pos]
+            if (valores[pos] > maior) maior = valores[pos]
+            if (valores[pos] < menor) menor = valores[pos]
+        }
+
+        var media = soma / total
+
+        res.innerHTML = ''
+        res.innerHTML += `<p>Ao todo temos ${total} números cadastrados</p>`
+        res.innerHTML += `<p>O maior valor foi ${maior}</p>`
+        res.innerHTML += `<p>O menor valor foi ${menor}</p>`
+        res.innerHTML += `<p>A soma dos valores é ${soma}</p>`
+        res.innerHTML += `<p>A média dos valores é ${media}</p>`
+    }
 }
