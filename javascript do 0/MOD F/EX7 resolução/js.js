5b780f43e393f1993f0f651bb1477d1cd8eae15d
Diego R <diegorolimdejesus@gmail.com>
2026-01-16 11:30:54 -0300

    ok

--- javascript do 0/MOD F/EX7 resolução/js.js
+++ javascript do 0/MOD F/EX7 resolução/js.js
@@ -21,8 +21,43 @@ function inLista (n, l) {
 
 function adicionar(){
      if(isNumero(num.value) && !inLista(num.value, valores)) {
-        alert('tudo ok')
+        valores.push(Number(num.value))
+        var item = document.createElement('option')
+        item.text = `Valor ${num.value} adicionado`
+        lista.appendChild(item)
+        res.innerHTML = ''
 } else {
     alert('valor ja inserido ou invalido')
  }
+ num.value = ''
+ num.focus()
 }
+
+function finalizar() {
+    if (valores.length == 0){
+        alert('escreva algo')
+    } else {
+        var tot = valores.length
+        var maior = valores[0]
+        var menor = valores[0]
+        var soma = 0
+        var media = 0
+        for (var pos in valores) {
+            soma += valores[pos]
+            if (valores[pos] > maior)
+                maior = valores[pos] 
+            if (valores[pos] < menor)
+                menor = valores[pos]
+        }
+        media = soma / tot
+        res.innerHTML = ''
+        res.innerHTML += `<p>Ao todo temos ${tot} numeros cadastrados.</p>`
+        res.innerHTML += `<p>O maior valor é ${maior}.</p>`
+        res.innerHTML += `<p>O menor valor é ${menor}. </p>`
+        res.innerHTML += `<p>Somando todos os valores temos ${soma}. </p>`
+        res.innerHTML += `<p>A media dos valores é ${media} </p>`
+
+    }
+}
+
+//res.innerHTML += `<p> </p>`
