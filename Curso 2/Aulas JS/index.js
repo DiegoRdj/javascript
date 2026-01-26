6be62bff86f9c713da4d7df5289bb9c18904fb28
Diego R <diegorolimdejesus@gmail.com>
2026-01-26 11:23:43 -0300

    bump

---
+++ Curso 2/Aulas JS/index.js
@@ -0,0 +1,17 @@
+//document.getElementById('texto').innerHTML=
+//'Meu primeiro texto <b>JS</b>!';
+//alert(5 + 5);
+//console.log('oi, isso é um console.log');
+
+
+//let a, b, c; a=5; b=6; c=7;
+
+//var a = 1
+//var b = 5
+//var c = a + b
+//console.log(c)
+
+//var PESSOA = 'Nome'
+//var pessoa = 'Sobrenome'
+//console.log(pessoa)
+//console.log(PESSOA)
