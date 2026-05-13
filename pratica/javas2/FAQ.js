7aae8b6bebdb02b9e46d55c26f74cacffe045759
DiegoRdj <diegorolimdejesus@gmail.com>
2026-05-13 13:56:27 -0300

    asdf

--- pratica/javas2/FAQ.js
+++ pratica/javas2/FAQ.js
@@ -6,60 +6,94 @@ let img1 = document.getElementById("mudarImg1")
 let img2 = document.getElementById("mudarImg2")
 let img3 = document.getElementById("mudarImg3")
 let img4 = document.getElementById("mudarImg4")
+let aberto = false;
+
 function botao1() {
+    if (aberto == false){
+        aberto = true
+        texto1.style.display = "block";
+        img1.style.display = "block";
      texto1.style.padding = "10px 40px"
     texto1.style.fontSize = "1.1em"
     texto1.style.color = "rgba(53, 53, 53, 0.705)"
     texto1.innerHTML = "Frontend Mentor offers realistic coding challenges to help developers improve their frontend coding skills with projects in HML, CSS, and JavaScript. It's suitable for all levels and ideal for portfolio building."
     img1.src = "faq-accordion-main/faq-accordion-main/assets/images/icon-minus.svg"
-}
-function botaoS1(){
-    
-   
+} else {
+
+        aberto = false
+
+        texto1.style.display = "none";
+      
+
     texto1.style.color = "rgba(53, 53, 53, 0.705)"
     texto1.innerHTML= ""
     img1.src = "faq-accordion-main/faq-accordion-main/assets/images/icon-plus.svg"
-    
+}    
 }
 function botao2() {
-     texto2.style.padding = "10px 40px"
+
+    if (aberto == false) {
+        aberto = true
+        texto2.style.display = "block";
+        img2.style.display = "block";
+
+    texto2.style.padding = "10px 40px"
     texto2.style.fontSize = "1.1em"
     texto2.style.color = "rgba(53, 53, 53, 0.705)"
     texto2.innerHTML = "Yes, Frontend Mentor offers both free and premium coding challenges, with the free option providing access to a range of projects suitable for all skill levels."
     img2.src = "faq-accordion-main/faq-accordion-main/assets/images/icon-minus.svg"
-}
-function botaoS2(){
+} else {
+
+    aberto = false 
+    texto2.style.display = "none"
 
     texto2.style.color = "rgba(53, 53, 53, 0.705)"
     texto2.innerHTML= ""
     img2.src = "faq-accordion-main/faq-accordion-main/assets/images/icon-plus.svg"
 }
+}
 function botao3() {
+
+    if(aberto == false) {
+
+        aberto = true
+        texto3.style.display = "block"
+        img3.style.display = "block"
+
      texto3.style.padding = "10px 40px"
     texto3.style.fontSize = "1.1em"
     texto3.style.color = "rgba(53, 53, 53, 0.705)"
       texto3.innerHTML = "Yes, you can use projects completed on Frontend Mentor in your portfolio. It's an excellent way to showcase your skills to potential employers!"
     img3.src = "faq-accordion-main/faq-accordion-main/assets/images/icon-minus.svg"
-}
-function botaoS3(){
-    
-  
+} else {
+
+    aberto = false
+    texto3.style.display = "none"
+
     texto3.style.color = "rgba(53, 53, 53, 0.705)"
     texto3.innerHTML= ""
     img3.src = "faq-accordion-main/faq-accordion-main/assets/images/icon-plus.svg"
 }
+}
 function botao4() {
+    if(aberto === false) {
+
+        aberto = true
+        texto4.style.display = "block"
+        img4.style.display = "block"
+
      texto4.style.padding = "0px 40px 20px 40px"
     texto4.style.fontSize = "1.1em"
     texto4.style.color = "rgba(53, 53, 53, 0.705)"
      texto4.innerHTML = "The best place to get help is inside Frontend Mentor's Discord community. There's a help channel where you can ask questions and seek support from other community members."
     img4.src = "faq-accordion-main/faq-accordion-main/assets/images/icon-minus.svg"
-}
-function botaoS4(){
-    
+} else {
+
+    aberto = false
+    texto4.style.display = "none"
    
     texto4.style.color = "rgba(53, 53, 53, 0.705)"
     texto4.innerHTML= ""
     img4.src = "faq-accordion-main/faq-accordion-main/assets/images/icon-plus.svg"
-    
+}    
 }
